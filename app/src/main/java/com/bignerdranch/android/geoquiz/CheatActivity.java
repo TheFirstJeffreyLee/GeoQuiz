ac0c65fd396ca1206317217e1acac259d67b55d5
Jeffrey Lee <jlee@spokeo.com>
2016-10-17 16:27:08 -0700

    finished ch 6

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -26,12 +30,30 @@ public class CheatActivity extends AppCompatActivity {
         mShowAnswer.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                if (mAnswerIsTrue) {
-                    mAnswerTextView.setText(R.string.true_button);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                    if (mAnswerIsTrue) {
+                        mAnswerTextView.setText(R.string.true_button);
+                    } else {
+                        mAnswerTextView.setText(R.string.false_button);
+                    }
+                    setAnswerShownResult(true);
+
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
                 } else {
-                    mAnswerTextView.setText(R.string.false_button);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
                 }
-                setAnswerShownResult(true);
             }
         });
 
